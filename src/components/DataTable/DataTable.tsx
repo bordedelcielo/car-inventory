bed0d64f8d4de7b59612ff1f328c08a505dfbeb1
bordedelcielo <christopher.thrutchley@gmail.com>
2021-11-29 23:19:47 -0600

    CRUD ops working

--- src/components/DataTable/DataTable.tsx
+++ src/components/DataTable/DataTable.tsx
@@ -8,12 +8,19 @@
 //     )
 // }
 
-import * as React from 'react';
+import React, { useState } from 'react';
 // import { DataGrid, GridColDef, GridValueGetterParams } from '@material-ui/data-grid';
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import { DataGrid, GridColDef, GridSelectionModel, GridValueGetterParams } from '@mui/x-data-grid';
 import { server_calls } from '../../api';
 import { useGetData } from '../../custom-hooks';
-
+import { Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle
+} from "@mui/material";
+import { CarForm } from '../../components';
 
 const columns: GridColDef[] = [
     { field: 'id', headerName: 'ID', width: 70 },
@@ -51,12 +58,61 @@ const columns: GridColDef[] = [
   //   { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
   // ];
 
+  interface gridData{
+    data:{
+      id?:string;
+      
+    }
+  }
+
   export const DataTable = () => {
       let { carData, getData } = useGetData(); // check to see if carData vs droneData
+      let [open, setOpen] = useState(false);
+      let [gridData, setData] = useState<GridSelectionModel>([])
+
+      let handleOpen = () =>{
+        setOpen(true)
+      }
+
+      let handleClose = () =>{
+        setOpen(false)
+      }
+
+      let deleteData = async () =>{
+        await server_calls.delete(`${gridData[0]}`)
+        getData()
+      }
+
+      console.log(gridData) // a list of the boxes checked
+
       return (
           <div style={{ height: 400, width: '100%'}}>
               <h2>Cars In Inventory</h2>
-              <DataGrid rows={carData} columns={columns} pageSize={5} checkboxSelection />
+              <DataGrid rows={carData} 
+                        columns={columns} 
+                        pageSize={5} 
+                        checkboxSelection
+                        onSelectionModelChange= { (newSelectionModel) =>{setData(newSelectionModel);}} 
+                        {...carData}
+                        />
+              <Button onClick={handleOpen}>Update</Button>
+              <Button variant='contained' color='error' onClick={deleteData}>Delete</Button>
+
+              {/* Dialog Popup Start */}
+              <Dialog open={open} onClose={handleClose} aria-labelledby='form-dialog-title'>
+                <DialogTitle id='form-dialog-title'>Update a Car</DialogTitle>
+                <DialogContent>
+                  <DialogContentText>
+                    Updating: {gridData[0]}
+                  </DialogContentText>
+                    <CarForm id={`${gridData[0]}`}/>
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={handleClose} style={{backgroundColor:'maroon'}}>Cancel</Button>
+                </DialogActions>
+              </Dialog>
+
+
           </div>
       )
   }
